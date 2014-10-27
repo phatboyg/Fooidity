3aaca4c71d1f61b8235e478bb134e1328e6be587
Chris Patterson <chris@phatboyg.com>
2014-10-26 20:10:07 -0700

    Formatting

--- src/Fooidity/DateRangeCodeSwitch.cs
+++ src/Fooidity/DateRangeCodeSwitch.cs
@@ -2,14 +2,15 @@
 {
     using System;
 
+
     public class DateRangeCodeSwitch<T> :
         CodeSwitch<T>
         where T : struct, CodeFeature
     {
+        readonly Func<DateTime> _currentTime;
         readonly bool _enabled;
         readonly DateTime? _enabledFromDate;
         readonly DateTime? _enabledToDate;
-        readonly Func<DateTime> _currentTime;
 
         public DateRangeCodeSwitch(bool enabled,
             DateTime? enabledFromDate,
@@ -26,19 +27,16 @@
         {
             get
             {
-                if (!_enabled) return false;
+                if (!_enabled)
+                    return false;
 
                 DateTime now = _currentTime();
 
                 if (_enabledFromDate.HasValue && (now < _enabledFromDate.Value))
-                {
                     return false;
-                }
 
                 if (_enabledToDate.HasValue && (now > _enabledToDate.Value))
-                {
                     return false;
-                }
 
                 return true;
             }
