9d4cf9d2581fd18ba95003aa6115b43e192ace2e
Dru Sellers <dru@drusellers.com>
2014-10-26 21:29:21 -0500

    Adding a DateRange switch and Shouldly

---
+++ src/Fooidity.Tests/A_disabled_DateRange.cs
@@ -0,0 +1,47 @@
+﻿namespace Fooidity.Tests
+{
+    using System;
+    using NUnit.Framework;
+    using Shouldly;
+
+    public class A_disabled_DateRange
+    {
+        [Test]
+        public void Enabled_and_correct_dates()
+        {
+            var start = new DateTime(2014, 1, 1);
+            var end = new DateTime(2014, 2, 1);
+            Func<DateTime> current = ()=> new DateTime(2014,1,15);
+            var x = new DateRangeCodeSwitch<SpecialDiscount>(true, start, end, current);
+
+            x.Enabled.ShouldBe(true);
+        }
+
+        [Test]
+        public void Disabled_even_if_date_is_right()
+        {
+            var start = new DateTime(2014, 1, 1);
+            var end = new DateTime(2014, 2, 1);
+            Func<DateTime> current = () => new DateTime(2014, 1, 15);
+            var x = new DateRangeCodeSwitch<SpecialDiscount>(false, start, end, current);
+
+            x.Enabled.ShouldBe(false);
+        }
+
+        [Test]
+        public void Disabled_if_date_is_incorrect()
+        {
+            var start = new DateTime(2014, 1, 1);
+            var end = new DateTime(2014, 2, 1);
+            Func<DateTime> current = () => new DateTime(2014, 3, 15);
+            var x = new DateRangeCodeSwitch<SpecialDiscount>(true, start, end, current);
+
+            x.Enabled.ShouldBe(false);
+        }
+
+        struct SpecialDiscount : CodeFeature
+        {
+            
+        }
+    }
+}
---
+++ src/Fooidity/DateRangeCodeSwitch.cs
@@ -0,0 +1,47 @@
+﻿namespace Fooidity
+{
+    using System;
+
+    public class DateRangeCodeSwitch<T> :
+        CodeSwitch<T>
+        where T : struct, CodeFeature
+    {
+        readonly bool _enabled;
+        readonly DateTime? _enabledFromDate;
+        readonly DateTime? _enabledToDate;
+        readonly Func<DateTime> _currentTime;
+
+        public DateRangeCodeSwitch(bool enabled,
+            DateTime? enabledFromDate,
+            DateTime? enabledToDate,
+            Func<DateTime> currentTime)
+        {
+            _enabled = enabled;
+            _enabledFromDate = enabledFromDate;
+            _enabledToDate = enabledToDate;
+            _currentTime = currentTime;
+        }
+
+        public bool Enabled
+        {
+            get
+            {
+                if (!_enabled) return false;
+
+                DateTime now = _currentTime();
+
+                if (_enabledFromDate.HasValue && (now < _enabledFromDate.Value))
+                {
+                    return false;
+                }
+
+                if (_enabledToDate.HasValue && (now > _enabledToDate.Value))
+                {
+                    return false;
+                }
+
+                return true;
+            }
+        }
+    }
+}
